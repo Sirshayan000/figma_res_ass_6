996afc673eb496c63a9189a2a137fd064ba57b88
Sirshayan000 <sirshan000@gmail.com>
2024-11-23 21:06:06 +0500

    yor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-      },
+      },                                                                        
+
+      screen:{
+        mobile:{"max-w":"1000px"}
+      }
     },
   },
   plugins: [],
